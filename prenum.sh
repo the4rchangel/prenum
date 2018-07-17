f8cd1c004c6d2925a7602d8fb9806921dc7b8e71
ICMPofDED <32465454+ICMPofDED@users.noreply.github.com>
2018-07-16 23:58:17 -0700

    Create prenum.sh

---
+++ prenum.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# basic enum script created by @ping_of_death_
+# http://hired.team/
+# 
+for ip in "$@"
+do
+echo '[elevator music plays softly]'
+echo '****************************************************' > "$1"
+echo '****************Unicorn Scan************************' >> "$1"
+echo '****************************************************' >> "$1"
+echo '**********Currently doing Unicorn Scan**************'
+#echo $1
+unicornscan "$1" >> "$1"
+echo '****************************************************' >> "$1"
+echo '**********************NMAP**************************' >> "$1"
+echo '****************************************************' >> "$1"
+echo '*************Currently doing NMAP*******************'
+echo '[elevator music intensifies]'
+nmap \-T4 \-Pn \-sV \-A "$1" >> "$1"
+echo '****************************************************' >> "$1"
+echo '*********************Curl***************************' >> "$1"
+echo '****************************************************' >> "$1"
+echo '**************Currently curling*********************'
+echo '[elevator music switches to death metal]'
+echo 'You have arrived at your destination. Check the current working directory for the file with the same name as the ip of your target'
+curl \-\-silent "$1" | html2text >> "$1"
+done
